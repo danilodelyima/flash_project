fc2e80729a2b6a9a943cb058b4495e3b1ec6a11b
Danilo <danilodelyima@gmail.com>
2018-06-10 17:55:11 -0300

    add seed and set before save to topic

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,13 @@
 class Topic < ApplicationRecord
   belongs_to :classroom
+  before_save :set_confidence, :set_reviewed
+
+  def set_confidence
+    self.confidence ||= 0
+  end
+
+  def set_reviewed
+    self.reviewed ||= 0
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(email:'danilo@email.com', password:'123456')
+danilo = User.create(email:'danilo@email.com', password:'123456')
 User.create(email:'teste@email.com', password:'123456')
+
+
+calculo_1 = Classroom.create(name:'Cálculo 1',
+                             config_review:'1-2-7-30',
+                             user_id: danilo.id)
+
+limite = Topic.create(name:'Limite', classroom_id: calculo_1.id)
+Topic.create(name:'Derivada', classroom_id: calculo_1.id)
+Topic.create(name:'Integral', classroom_id: calculo_1.id)
+
+Flashcard.create(question: 'Qual o limite de uma constante?',
+                 answer: 'É a própria constante',
+                 correct: true,
+                 topic_id: limite.id)
